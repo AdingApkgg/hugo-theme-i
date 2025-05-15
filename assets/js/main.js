562fb105addbf0f76e8ce8261f82abfa2233d371
AdingApkgg <AdingApkgg@outlook.com>
2025-05-16 02:53:00 +0800

    250516

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,27 +62,29 @@ function initMediumZoom() {
     margin: 0,
     background: "transparent",
     scrollOffset: 20,
-  })
+  });
 }
 
 function initGalPopup() {
-  const tAgeVerification = document.getElementById('tAgeVerification');
+  const tAgeVerification = document.getElementById("tAgeVerification");
   if (!tAgeVerification) {
-    console.error('Age verification translations not found');
+    console.error("Age verification translations not found");
     return;
   }
-  const text = tAgeVerification.dataset.text || '本サイトは18歳以上の方を対象としています。<br>あなたは18歳以上ですか？';
-  const yes = tAgeVerification.dataset.yes || 'はい';
-  const no = tAgeVerification.dataset.no || 'いいえ';
+  const text =
+    tAgeVerification.dataset.text ||
+    "本サイトは18歳以上の方を対象としています。<br>あなたは18歳以上ですか？";
+  const yes = tAgeVerification.dataset.yes || "はい";
+  const no = tAgeVerification.dataset.no || "いいえ";
   const locationHref = tAgeVerification.dataset.url;
 
-  const ageVerificationTime = localStorage.getItem('ageVerificationTime');
+  const ageVerificationTime = localStorage.getItem("ageVerificationTime");
   const oneWeek = 3 * 24 * 60 * 60 * 1000;
   const now = new Date().getTime();
   if (!ageVerificationTime || now - parseInt(ageVerificationTime) > oneWeek) {
     Swal.fire({
       text: text,
-      icon: 'warning',
+      icon: "warning",
       showConfirmButton: true,
       confirmButtonText: yes,
       showDenyButton: true,
@@ -94,7 +96,7 @@ function initGalPopup() {
       background: "var(--bg-color)",
     }).then((result) => {
       if (result.isConfirmed) {
-        localStorage.setItem('ageVerificationTime', now.toString());
+        localStorage.setItem("ageVerificationTime", now.toString());
       } else {
         window.location.href = locationHref;
       }
@@ -194,27 +196,28 @@ observer.observe(document, {
 
 function lunar() {
   var lunarInfo = [
-    19416, 19168, 42352, 21717, 53856, 55632, 91476, 22176, 39632, 21970, 19168,
-    42422, 42192, 53840, 119381, 46400, 54944, 44450, 38320, 84343, 18800,
-    42160, 46261, 27216, 27968, 109396, 11104, 38256, 21234, 18800, 25958,
-    54432, 59984, 28309, 23248, 11104, 100067, 37600, 116951, 51536, 54432,
-    120998, 46416, 22176, 107956, 9680, 37584, 53938, 43344, 46423, 27808,
-    46416, 86869, 19872, 42416, 83315, 21168, 43432, 59728, 27296, 44710, 43856,
-    19296, 43748, 42352, 21088, 62051, 55632, 23383, 22176, 38608, 19925, 19152,
-    42192, 54484, 53840, 54616, 46400, 46752, 103846, 38320, 18864, 43380,
-    42160, 45690, 27216, 27968, 44870, 43872, 38256, 19189, 18800, 25776, 29859,
-    59984, 27480, 23232, 43872, 38613, 37600, 51552, 55636, 54432, 55888, 30034,
-    22176, 43959, 9680, 37584, 51893, 43344, 46240, 47780, 44368, 21977, 19360,
-    42416, 86390, 21168, 43312, 31060, 27296, 44368, 23378, 19296, 42726, 42208,
-    53856, 60005, 54576, 23200, 30371, 38608, 19195, 19152, 42192, 118966,
-    53840, 54560, 56645, 46496, 22224, 21938, 18864, 42359, 42160, 43600,
-    111189, 27936, 44448, 84835, 37744, 18936, 18800, 25776, 92326, 59984,
-    27424, 108228, 43744, 41696, 53987, 51552, 54615, 54432, 55888, 23893,
-    22176, 42704, 21972, 21200, 43448, 43344, 46240, 46758, 44368, 21920, 43940,
-    42416, 21168, 45683, 26928, 29495, 27296, 44368, 84821, 19296, 42352, 21732,
-    53600, 59752, 54560, 55968, 92838, 22224, 19168, 43476, 41680, 53584, 62034,
-    54560,
-  ],
+      19416, 19168, 42352, 21717, 53856, 55632, 91476, 22176, 39632, 21970,
+      19168, 42422, 42192, 53840, 119381, 46400, 54944, 44450, 38320, 84343,
+      18800, 42160, 46261, 27216, 27968, 109396, 11104, 38256, 21234, 18800,
+      25958, 54432, 59984, 28309, 23248, 11104, 100067, 37600, 116951, 51536,
+      54432, 120998, 46416, 22176, 107956, 9680, 37584, 53938, 43344, 46423,
+      27808, 46416, 86869, 19872, 42416, 83315, 21168, 43432, 59728, 27296,
+      44710, 43856, 19296, 43748, 42352, 21088, 62051, 55632, 23383, 22176,
+      38608, 19925, 19152, 42192, 54484, 53840, 54616, 46400, 46752, 103846,
+      38320, 18864, 43380, 42160, 45690, 27216, 27968, 44870, 43872, 38256,
+      19189, 18800, 25776, 29859, 59984, 27480, 23232, 43872, 38613, 37600,
+      51552, 55636, 54432, 55888, 30034, 22176, 43959, 9680, 37584, 51893,
+      43344, 46240, 47780, 44368, 21977, 19360, 42416, 86390, 21168, 43312,
+      31060, 27296, 44368, 23378, 19296, 42726, 42208, 53856, 60005, 54576,
+      23200, 30371, 38608, 19195, 19152, 42192, 118966, 53840, 54560, 56645,
+      46496, 22224, 21938, 18864, 42359, 42160, 43600, 111189, 27936, 44448,
+      84835, 37744, 18936, 18800, 25776, 92326, 59984, 27424, 108228, 43744,
+      41696, 53987, 51552, 54615, 54432, 55888, 23893, 22176, 42704, 21972,
+      21200, 43448, 43344, 46240, 46758, 44368, 21920, 43940, 42416, 21168,
+      45683, 26928, 29495, 27296, 44368, 84821, 19296, 42352, 21732, 53600,
+      59752, 54560, 55968, 92838, 22224, 19168, 43476, 41680, 53584, 62034,
+      54560,
+    ],
     solarMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
     Gan = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"],
     Zhi = [
@@ -530,7 +533,7 @@ function lunar() {
     return (
       "魔羯水瓶双鱼白羊金牛双子巨蟹狮子处女天秤天蝎射手魔羯".substr(
         2 * b -
-        (f < [20, 19, 21, 21, 21, 22, 23, 23, 23, 23, 22, 22][b - 1] ? 2 : 0),
+          (f < [20, 19, 21, 21, 21, 22, 23, 23, 23, 23, 22, 22][b - 1] ? 2 : 0),
         2
       ) + "座"
     );
@@ -657,7 +660,9 @@ function lunar() {
       T = null;
     y === c && ((M = !0), (T = solarTerm[2 * f - 2])),
       m === c && ((M = !0), (T = solarTerm[2 * f - 1]));
-    var I = toGanZhi(Date.UTC(b, g, 1, 0, 0, 0, 0) / 864e5 + 25567 + 10 + c - 1),
+    var I = toGanZhi(
+        Date.UTC(b, g, 1, 0, 0, 0, 0) / 864e5 + 25567 + 10 + c - 1
+      ),
       C = toAstro(f, c);
     return {
       lYear: l,
@@ -687,7 +692,10 @@ function lunar() {
     },
     lunar2solar: function (b, f, c, e) {
       if ((e = !!e) && leapMonth !== f) return -1;
-      if ((2100 === b && 12 === f && c > 1) || (1900 === b && 1 === f && c < 31))
+      if (
+        (2100 === b && 12 === f && c > 1) ||
+        (1900 === b && 1 === f && c < 31)
+      )
         return -1;
       var a = monthDays(b, f),
         r = a;
@@ -702,7 +710,11 @@ function lunar() {
       e && (t += a);
       var u = Date.UTC(1900, 1, 30, 0, 0, 0),
         o = new Date(864e5 * (t + c - 31) + u);
-      return solar2lunar(o.getUTCFullYear(), o.getUTCMonth() + 1, o.getUTCDate());
+      return solar2lunar(
+        o.getUTCFullYear(),
+        o.getUTCMonth() + 1,
+        o.getUTCDate()
+      );
     },
   };
 
@@ -720,8 +732,8 @@ function lunar() {
     if (sessionStorage.getItem("isPopupWindow") != "1") {
       Swal.fire(
         "今天是九一八事变" +
-        (y - 1931).toString() +
-        "周年纪念日\n🪔勿忘国耻，振兴中华🪔"
+          (y - 1931).toString() +
+          "周年纪念日\n🪔勿忘国耻，振兴中华🪔"
       );
       sessionStorage.setItem("isPopupWindow", "1");
     }
@@ -733,8 +745,8 @@ function lunar() {
     if (sessionStorage.getItem("isPopupWindow") != "1") {
       Swal.fire(
         "今天是卢沟桥事变" +
-        (y - 1937).toString() +
-        "周年纪念日\n🪔勿忘国耻，振兴中华🪔"
+          (y - 1937).toString() +
+          "周年纪念日\n🪔勿忘国耻，振兴中华🪔"
       );
       sessionStorage.setItem("isPopupWindow", "1");
     }
@@ -746,8 +758,8 @@ function lunar() {
     if (sessionStorage.getItem("isPopupWindow") != "1") {
       Swal.fire(
         "今天是南京大屠杀" +
-        (y - 1937).toString() +
-        "周年纪念日\n🪔勿忘国耻，振兴中华🪔"
+          (y - 1937).toString() +
+          "周年纪念日\n🪔勿忘国耻，振兴中华🪔"
       );
       sessionStorage.setItem("isPopupWindow", "1");
     }
